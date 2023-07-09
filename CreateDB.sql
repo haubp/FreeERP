087c9bda9c9cced675f8abdd23fc66b82845dbf9
haubp <buiphonghau2018@gmail.com>
2023-07-09 23:03:32 +0700

    Add sql script

---
+++ CreateDB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE User (
+	user_id int auto_increment primary key,
+	username varchar(50),
+	password varchar(50),
+	type varchar(50)
+);
+
+CREATE TABLE SaleTicket (
+	ticket_id int auto_increment primary key,
+	user_id int references User(user_id),
+	date_created DATETIME,
+	content varchar(50),
+	product varchar(50),
+);
+
+CREATE TABLE CustomerSuccessTicket (  
+        ticket_id int auto_increment primary key,
+        user_id int references User(user_id),
+        date_created DATETIME,
+        content varchar(50)
+);
+
+CREATE TABLE EngineerTicket (  
+        ticket_id int auto_increment primary key,
+        user_id int references User(user_id),
+        date_created DATETIME,
+        content varchar(50)
+);
