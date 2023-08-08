4fddb6b0ac6af0ae8e673124f39adb40f836cb11
haubp <buiphonghau2018@gmail.com>
2023-08-08 07:09:46 +0700

    study material

---
+++ ERP/Middlewares/MyCustomMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FreeERP.Middlewares
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom Middleware");
+            await next(context);
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyCustomMiddleware>();
+        }
+    }
+}
+
