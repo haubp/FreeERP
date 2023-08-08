b38fafcc2b5a9d602180c0784d82be8dabb32543
hau.bui <hau.bui@opswat.com>
2023-08-08 19:14:05 +0700

    route constraint

---
+++ ERP/Constraints/MonthsCustomConstraint.cs
@@ -0,0 +1,15 @@
+﻿namespace FreeERP.Constraints
+{
+    public class MonthsCustomConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!values.ContainsKey(routeKey))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
