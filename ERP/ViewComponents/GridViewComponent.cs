9b77547e4aa444f48dab1f8b9354e06e3c538731
haubp <buiphonghau2018@gmail.com>
2023-08-17 11:18:29 +0700

    view component

---
+++ ERP/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FreeERP.Model;
+
+namespace FreeERP.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int x, int y)
+        {
+            TestModel test = new ();
+            return View("Test", test);
+        }
+    }
+}
+
