2d0c56be5c0a45c823b68264a6a28baf73468716
haubp <buiphonghau2018@gmail.com>
2023-08-25 23:43:07 +0700

    country crud test

--- CRUDTests/CountriesServiceTest.cs
+++ CRUDTests/CountriesServiceTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ServiceContracts;
 using Entities;
 using Services;
+using ServiceContracts.DTO;
 
 namespace CRUDTests
 {
@@ -16,6 +17,75 @@ namespace CRUDTests
         public CountriesServiceTest()
         {
             _countriesService = new CountriesService();
+        }
+
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            // Arrange
+            CountryAddRequest? request = null;
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_CountryNameIsNull()
+        {
+            // Arrange
+            CountryAddRequest? request = new CountryAddRequest()
+            {
+                CountryName = null
+            };
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            // Arrange
+            CountryAddRequest? request1 = new CountryAddRequest()
+            {
+                CountryName = "VietNam"
+            };
+            CountryAddRequest? request2 = new CountryAddRequest()
+            {
+                CountryName = "VietNam"
+            };
+
+            // Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                // Act
+                _countriesService.AddCountry(request1);
+                _countriesService.AddCountry(request2);
+            });
+        }
+
+        [Fact]
+        public void AddCountry_ProperCountryDetails()
+        {
+            // Arrange
+            CountryAddRequest? request = new CountryAddRequest()
+            {
+                CountryName = "VietNam"
+            };
+
+            // Act
+            CountryResponse response = _countriesService.AddCountry(request);
+
+            // Assert
+            Assert.True(response.CountryID != Guid.Empty);
         }
     }
 }
