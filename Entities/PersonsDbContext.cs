99349c81e4c76b594c597e064f54e6e65db88f00
hau.bui <hau.bui@opswat.com>
2023-09-12 15:09:32 +0700

    tag helpers code

---
+++ Entities/PersonsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class PersonsDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Person>().ToTable("Persons");
+        }
+    }
+}
